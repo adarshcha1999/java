7be0bfe9107434f57bc8fb4138c6dd9c27207fd7
adarshcha1999 <adarshcha1999@gmail.com>
2024-06-27 10:57:23 +0530

    Map interface
    marks print with put mathod

---
+++ Mapexample.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class Mapexample{
+    public static void main(String[] args){
+       HashMap<String,Integer> marks=new HashMap<>();
+        marks.put("Adarsh",78);
+        marks.put("Akhilesh",75);
+        marks.put("Suraj",80);
+        marks.put("Sachin",70);
+        marks.put("Vivek",85);
+        System.out.println(marks);
+    }
+}
