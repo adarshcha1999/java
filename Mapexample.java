41d3b68a2d9c79e8ad4909c5d8282ed5c1b11b35
adarshcha1999 <adarshcha1999@gmail.com>
2024-06-27 11:01:48 +0530

    Update Mapexample.java

--- Mapexample.java
+++ Mapexample.java
@@ -8,5 +8,9 @@ public class Mapexample{
         marks.put("Sachin",70);
         marks.put("Vivek",85);
         System.out.println(marks);
+        for(String key : marks.keySet())
+        {
+            System.out.println(marks.get(key));
+        }
     }
 }
