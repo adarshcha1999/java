e8227ea1ebc15af01617fe2477cf4c8adf95c29c
adarshcha1999 <adarshcha1999@gmail.com>
2024-06-28 12:38:10 +0530

    Arraylist
    list of student name

---
+++ Student.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Student{
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Adarsh");
+        names.add("Akhilesh");
+        names.add("Suraj");
+        System.out.println("After Adding element:"+names);
+    }
+}
